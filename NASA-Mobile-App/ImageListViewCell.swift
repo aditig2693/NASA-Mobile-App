4f270fcf14ff18049411572f01c4e98f3b1a9ccb
Aditi <aditi25926@gmail.com>
2023-05-07 14:10:41 +0100

    Initial Commit

---
+++ NASA-Mobile-App/ImageListViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageListViewCell.swift
+//  NASA-Mobile-App
+//
+//  Created by Aditi Patil on 07/05/2023.
+//
+
+import UIKit
+
+class ImageListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NASA-Mobile-AppUITests/NASA_Mobile_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NASA_Mobile_AppUITestsLaunchTests.swift
+//  NASA-Mobile-AppUITests
+//
+//  Created by Aditi Patil on 07/05/2023.
+//
+
+import XCTest
+
+final class NASA_Mobile_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
